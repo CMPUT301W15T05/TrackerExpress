7073fa2c12f4734ad424e731388997fc5fdfebd9
ParashRahman <parash@ualberta.ca>
2015-02-12 01:55:26 -0700

    Initiated EditExpenseActivityTest class

---
+++ TrackerExpressTestTest/src/EditExpenseActivityTest.java
@@ -0,0 +1,15 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class EditExpenseActivityTest extends
+		ActivityInstrumentationTestCase2<EditExpenseActivity> {
+
+	public EditExpenseActivityTest(String name) {
+		super(EditExpenseActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
