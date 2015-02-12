fbc70ba5175cecc8d5b98d6bd655484cdab41492
ParashRahman <parash@ualberta.ca>
2015-02-12 01:52:30 -0700

    Initiated ViewClaimActivityTest class

---
+++ TrackerExpressTestTest/src/ViewClaimActivityTest.java
@@ -0,0 +1,13 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ViewClaimActivityTest extends
+		ActivityInstrumentationTestCase2<ViewClaimActivity> {
+	
+	public ViewClaimActivityTest() {
+		super(ViewClaimActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+}
