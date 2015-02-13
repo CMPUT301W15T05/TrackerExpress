41469c36721ffed99b85959f78de586f0b46f3b6
ParashRahman <parash@ualberta.ca>
2015-02-12 20:15:03 -0700

    Added MainActivityTest (Can test list of Claims here )

---
+++ TrackerExpressTestTest/src/MainActivityTest.java
@@ -0,0 +1,16 @@
+import group5.trackerexpress.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+
+	public MainActivityTest(String name) {
+		super(MainActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
