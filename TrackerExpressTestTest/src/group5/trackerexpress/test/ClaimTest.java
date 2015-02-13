140b87ee6acece9ad83c081630bfe92374f57794
JCKE <jcemery@ualberta.ca>
2015-02-13 13:09:32 -0700

    Added claimtest class

---
+++ TrackerExpressTestTest/src/group5/trackerexpress/test/ClaimTest.java
@@ -0,0 +1,13 @@
+package group5.trackerexpress.test;
+
+import group5.trackerexpress.Claim;
+import junit.framework.TestCase;
+
+public class ClaimTest extends TestCase {
+
+	public void testSubmitClaim()
+	{
+		Claim claim = new Claim("testClaim");
+		Claim.setStatus
+	}
+}
