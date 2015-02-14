1b2ba138f1108bad86f34fb1ee28072a7e99a2b6
ParashRahman <parash@ualberta.ca>
2015-02-13 22:49:55 -0700

    Added the ReceiptTest (other time was false call)

---
+++ TrackerExpress/src/group5/trackerexpress/Receipt.java
@@ -0,0 +1,7 @@
+package group5.trackerexpress;
+
+import android.app.Activity;
+
+public class Receipt extends Activity{
+
+}
---
+++ TrackerExpressTestTest/src/group5/trackerexpress/test/ReceiptTest.java
@@ -0,0 +1,56 @@
+package group5.trackerexpress.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+
+import group5.trackerexpress.Receipt;
+
+import java.io.ByteArrayOutputStream;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ImageView;
+
+public class ReceiptTest extends
+		ActivityInstrumentationTestCase2<Receipt> {
+
+	public ReceiptTest() {
+		super(Receipt.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	// Test Case 10.1
+	public void testAddReceipt(){
+		takeAPhoto();
+		ImageView view = (ImageView) findViewById(R.id.receiptView);
+		// From http://stackoverflow.com/questions/4435806/drawable-to-byte accessed 2015-11-02
+		Drawable d = view.getDrawable();
+		Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+		byte[] bitmapdata = stream.toByteArray();
+		assertNotSame("Testing Existence of Picture", 0, bitmapdata.length);
+		assertTrue("Testing Image Size", bitmapdata.length<65536);
+	}
+	
+	// Test Case 11.1
+	public void testChangeReceipt(){
+		ImageView view = (ImageView) findViewById(R.id.receiptView);
+		Drawable original = view.getDrawable();
+		takeAPhoto();
+		Drawable changed = view.getDrawable();
+		assertNotSame("Testing Receipt Change", changed, original);
+	}
+	
+	// Test Case 12.1
+	public void testDeleteReceipt(){
+		ImageView view = (ImageView) findViewById(R.id.receiptView);
+		deletePhoto();
+		assertEquals("Testing Delete", null, view.getDrawable());
+	}
+}
