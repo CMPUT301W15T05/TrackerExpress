af3c3535b35f65aafaf123eacbb98658a908ac9b
sbaergen <sbaergen@ualberta.ca>
2015-03-03 09:05:49 -0700

    Added Receipt and User Test Classes and Classes

--- TrackerExpress/src/group5/trackerexpress/Receipt.java
+++ TrackerExpress/src/group5/trackerexpress/Receipt.java
@@ -3,5 +3,13 @@ package group5.trackerexpress;
 import android.app.Activity;
 
 public class Receipt extends Activity{
+	String path;
 
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
 }
