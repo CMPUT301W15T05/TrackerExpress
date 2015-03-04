ccdcbe4716c2ef67ca6491a0d09daeeab62b4a12
ParashRahman <parash@ualberta.ca>
2015-03-04 12:11:03 -0700

    ClaimList Test

--- TrackerExpressTestTest/src/group5/trackerexpress/test/ClaimListTest.java
+++ TrackerExpressTestTest/src/group5/trackerexpress/test/ClaimListTest.java
@@ -1,20 +1,15 @@
 package group5.trackerexpress.test;
 import group5.trackerexpress.Claim;
 import group5.trackerexpress.ClaimList;
+import group5.trackerexpress.Date;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.UUID;
 
 import junit.framework.TestCase;
 
 
 public class ClaimListTest extends TestCase {
-	
 	
 	
 	public void testEmptyClaimList() {
@@ -25,13 +20,13 @@ public class ClaimListTest extends TestCase {
 	
 	// this is test case for 01.01.01 and 01.02.01
 	public void testAddClaimList() {
-		
 		ClaimList claimList = new ClaimList();
 		String claimName = "A Claim";
 		Claim testClaim = new Claim(claimName);
 		claimList.add(testClaim);
 		assertTrue("Empty claim list", claimList.size()==1);
-		assertTrue("Test claim not contained", claimList.contains(testClaim));
+		assertTrue("Test claim not contained", 
+					testClaim.equals(claimList.getClaim(testClaim.getUuid())));
 	}
 	
 	
@@ -42,24 +37,30 @@ public class ClaimListTest extends TestCase {
 		Claim testClaim = new Claim(claimName);
 		
 		claimList.add(testClaim);
-		Collection<Claim> claim = claimList.getList();
 		
-		assertTrue("Empty claim list", claim.size()==1);
-		assertTrue("Test claim not contained", claim.contains(testClaim));
+		assertTrue("Empty claim list", claimList.size()==1);
+		assertTrue("Test claim not contained",
+					testClaim.equals( claimList.getClaim(testClaim.getUuid()) ));
 	}
 	
 	
 	// this is test case for 01.05.01
 	public void testRemoveClaimList() {
 		ClaimList claimList = new ClaimList();
-		String claimName = "A Claim";
-		Claim testClaim = new Claim(claimName);
+		Claim testClaim = new Claim("Test Claim");
+		UUID testUuid = testClaim.getUuid();
 		claimList.add(testClaim);
 		assertTrue("List size isn't big enough", claimList.size()==1);
-		assertTrue("Test claim not contained", claimList.contains(testClaim));
-		claimList.remove(testClaim);
+		assertTrue("Test claim not contained", 
+					testClaim.equals(claimList.getClaim(testClaim.getUuid())));
+		claimList.deleteClaim(testClaim.getUuid());
 		assertTrue("List size isn't small enough", claimList.size()==0);
-		assertFalse("test claim still contained", claimList.contains(testClaim));
+		try{
+			claimList.getClaim(testUuid);
+			assertTrue("This is not supposed to have happened", false);
+		} catch ( IndexOutOfBoundsException e ){
+			assertTrue("This is supposed to happen", true);
+		}
 	}
 	
 	// this is test case for 02.02.01 and 02.01.01
@@ -71,22 +72,10 @@ public class ClaimListTest extends TestCase {
 		Claim c2 = new Claim("1");
 		Claim c3 = new Claim("2");
 		
-		GregorianCalendar d1 = new GregorianCalendar();
-		GregorianCalendar d2 = new GregorianCalendar();
-		GregorianCalendar d3 = new GregorianCalendar();
 		
-		d1.set(Calendar.YEAR, 1995);
-		d1.set(Calendar.MONTH, 11); // december = 11
-		d1.set(Calendar.DAY_OF_MONTH, 12);
-		
-		d2.set(Calendar.YEAR, 1995);
-		d2.set(Calendar.MONTH, 0); // january = 0
-		d2.set(Calendar.DAY_OF_MONTH, 12);
-		
-		d3.set(Calendar.YEAR, 1995);
-		d3.set(Calendar.MONTH, 1); // february = 1
-		d3.set(Calendar.DAY_OF_MONTH, 12);
-		
+		Date d1 = new Date(1995, 12, 12);
+		Date d2 = new Date(1995, 1, 12);
+		Date d3 = new Date(1995, 2, 12);
 		
 		c1.setStartDate(d1);
 		c2.setStartDate(d2);
@@ -97,9 +86,9 @@ public class ClaimListTest extends TestCase {
 		claimList.add(c3);
 		
 		assertTrue("List size isn't correct", claimList.size()==3);
-		assertTrue("Claim '1' is not first", claimList.getList().get(0).getTitle().equals("1") );
-		assertTrue("Claim '2' is not second", claimList.getList().get(1).getTitle().equals("2") );
-		assertTrue("Claim '3' is not last", claimList.getList().get(2).getTitle().equals("3") );
+		assertTrue("Claim '1' is not first", claimList.getClaimList().get(0).getClaimName().equals("1") );
+		assertTrue("Claim '2' is not second", claimList.getClaimList().get(1).getClaimName().equals("2") );
+		assertTrue("Claim '3' is not last", claimList.getClaimList().get(2).getClaimName().equals("3") );
 
 	}
 	
