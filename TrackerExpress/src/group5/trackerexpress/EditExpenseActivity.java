efd759a1543b16304b5ff4705272cfa9b64473d8
iron_cupcakes <barnwal.rishi@gmail.com>
2015-03-11 01:20:09 -0600

    Added code for image button

--- TrackerExpress/src/group5/trackerexpress/EditExpenseActivity.java
+++ TrackerExpress/src/group5/trackerexpress/EditExpenseActivity.java
@@ -1,7 +1,84 @@
 package group5.trackerexpress;
 
+import java.io.File;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class EditExpenseActivity extends Activity{
+public class EditExpenseActivity extends Activity {
+
+	EditText description, amount;
+	Uri receiptUri;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_expense);
+
+		ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
+		OnClickListener listener = new OnClickListener() {
+			public void onClick(View v) {
+				takeAPhoto();
+			}
+		};
+		button.setOnClickListener(listener);
+	}
+
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+	public void takeAPhoto() {
+		// TODO: Create an intent with the action
+		// MediaStore.ACTION_IMAGE_CAPTURE
+
+		// ComponentName cn = new ComponentName("es.softwareprocess.bogopicgen",
+		// "es.softwareprocess.bogopicgen.BogoPicGenActivity");
+		// ComponentName cn = new ComponentName("com.android.camera",
+		// "com.android.camera.Camera");
+		// intent.setComponent(cn);
+
+		// Create a folder to store pictures
+		String folder = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/tmp";
+		File folderF = new File(folder);
+		if (!folderF.exists()) {
+			folderF.mkdir();
+
+		ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
+		Drawable photo = Drawable.createFromPath(receiptUri.getPath());
+		button.setImageDrawable(photo);
+		}
+
+		// Create an URI for the picture file
+		String imageFilePath = folder + "/"
+				+ String.valueOf(System.currentTimeMillis()) + ".jpg";
+		File imageFile = new File(imageFilePath);
+		receiptUri = Uri.fromFile(imageFile);
+
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, receiptUri);
+
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+		// TODO: Put in the intent in the tag MediaStore.EXTRA_OUTPUT the URI
+
+		// TODO: Start the activity (expecting a result), with the code
+		// CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE
+
+	}
+
+	private void initializeVars() {
+		// TODO Auto-generated method stub
+		description = (EditText) findViewById(R.id.editDescription);
+		amount = (EditText) findViewById(R.id.editAmount);
+	}
 
 }
