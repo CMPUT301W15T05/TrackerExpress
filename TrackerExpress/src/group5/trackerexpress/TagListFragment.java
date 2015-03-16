07ecbf073cf44957b70bf411c0b330050ac4be20
sbaergen <sbaergen@ualberta.ca>
2015-03-16 14:33:43 -0600

    Fixed rename tag

--- TrackerExpress/src/group5/trackerexpress/TagListFragment.java
+++ TrackerExpress/src/group5/trackerexpress/TagListFragment.java
@@ -29,28 +29,31 @@ import android.widget.AdapterView.OnItemLongClickListener;
  */
 public class TagListFragment extends Fragment implements TView {
 	/**
-	 * The fragment argument representing the section number for this
-	 * fragment.
+	 * The fragment argument representing the section number for this fragment.
 	 */
 	public static final String ARG_SECTION_NUMBER = "section_number";
 
 	/** The lv_tag_list. */
 	private ListView lv_tag_list;
-	
+
 	/** The value. */
 	private Editable value = null;
-	
+
 	/** The b_add_tag. */
 	private Button b_add_tag;
-	
+
 	/**
 	 * Instantiates a new tag list fragment.
 	 */
 	public TagListFragment() {
 	}
 
-	/* (non-Javadoc)
-	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater,
+	 * android.view.ViewGroup, android.os.Bundle)
 	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,88 +63,86 @@ public class TagListFragment extends Fragment implements TView {
 
 		lv_tag_list = (ListView) rootView.findViewById(R.id.lv_tags);
 		lv_tag_list.setItemsCanFocus(true);
-		
+
 		this.update(null);
-		
+
 		final TagMap mapOfTags = getTagMap(getActivity());
 		ArrayList<Tag> listOfTags = mapOfTags.getTags();
-		
+
 		mapOfTags.addView(this);
 		getTagMap(getActivity()).addView(this);
-		
+
 		b_add_tag = (Button) rootView.findViewById(R.id.b_add_tag);
-		
-		b_add_tag.setOnClickListener(new Button.OnClickListener(){
-		    public void onClick(View v) {
-		    	getName();
-		    	/*
-		    	if ( value != null ){
-		    		String name = value.toString();
-		    		Tag newTag = new Tag(name);
-		    		mapOfTags.addTag(getActivity(), newTag);
-		    		//listOfTags.add(newTag);
-		    		//MainTagListAdapter a = new MainTagListAdapter( myContext, listOfTags );
-        			lv_tag_list.setAdapter(a);
-		    	}
-		    	
-		    	value = null;
-		    	*/
-		    }
+
+		b_add_tag.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				Tag t = new Tag("");
+				getName(t, false);
+				/*
+				 * if ( value != null ){ String name = value.toString(); Tag
+				 * newTag = new Tag(name); mapOfTags.addTag(getActivity(),
+				 * newTag); //listOfTags.add(newTag); //MainTagListAdapter a =
+				 * new MainTagListAdapter( myContext, listOfTags );
+				 * lv_tag_list.setAdapter(a); }
+				 * 
+				 * value = null;
+				 */
+			}
 		});
-		
+
 		// Item click listener
-        lv_tag_list.setOnItemLongClickListener( new OnItemLongClickListener() {
-        	@Override
-			public boolean onItemLongClick(AdapterView<?> parent,
-					View view, final int position, long id) {
-				
+		lv_tag_list.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					final int position, long id) {
+
 				PopupMenu popup = new PopupMenu(getActivity(), view);
-				popup.getMenuInflater().inflate(R.menu.tag_list_popup, popup.getMenu());
-				
+				popup.getMenuInflater().inflate(R.menu.tag_list_popup,
+						popup.getMenu());
+
 				// Popup menu item click listener
 				popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                    	Tag t = (Tag) lv_tag_list.getAdapter().getItem(position);
-                    	
-                        switch(item.getItemId()){
-                        case R.id.op_delete_tag: 
-                        	// Delete tag off of Tag ArrayList for listview
-                        	//listOfTags.remove(t);
-                        	//MainTagListAdapter a = new MainTagListAdapter( getActivity().getBaseContext(), listOfTags );
-                			//lv_tag_list.setAdapter(a);
-                			// Delete it off the model
-                        	mapOfTags.deleteTag(getActivity(), t.getUuid());
-                        	break;
-                        case R.id.op_edit_tag:
-                        	getName();
-                        	
-                        	if ( value != null ){
-                        		String name = value.toString();
-                        		t.rename(getActivity(), name);
-                        	}
-                        	
-                        	value = null;
-                        	break;
-                        default: break;
-                        }
-                    	
-                        return true;
-                    }
-                });
-				
-	            popup.show();
+					public boolean onMenuItemClick(MenuItem item) {
+						Tag t = (Tag) lv_tag_list.getAdapter()
+								.getItem(position);
+
+						switch (item.getItemId()) {
+						case R.id.op_delete_tag:
+							// Delete tag off of Tag ArrayList for listview
+							// listOfTags.remove(t);
+							// MainTagListAdapter a = new MainTagListAdapter(
+							// getActivity().getBaseContext(), listOfTags );
+							// lv_tag_list.setAdapter(a);
+							// Delete it off the model
+							mapOfTags.deleteTag(getActivity(), t.getUuid());
+							break;
+						case R.id.op_edit_tag:
+							getName(t, true);
+							Log.v("Test", "Hello");
+							value = null;
+							break;
+						default:
+							break;
+						}
+
+						return true;
+					}
+				});
+
+				popup.show();
 				return false;
 			}
-        });
-        Log.i("myMessage", Integer.toString(listOfTags.size()));
+		});
+		Log.i("myMessage", Integer.toString(listOfTags.size()));
 
 		return rootView;
 	}
-	
+
 	/**
 	 * Gets the tag map.
 	 *
-	 * @param context the context
+	 * @param context
+	 *            the context
 	 * @return the tag map
 	 */
 	private TagMap getTagMap(Context context) {
@@ -153,46 +154,56 @@ public class TagListFragment extends Fragment implements TView {
 	 *
 	 * @return the name
 	 */
-	private void getName(){
+	private void getName(final Tag t, final boolean edit) {
 		String message = "Enter a new name";
-		final AutoCompleteTextView input = new AutoCompleteTextView(getActivity());
+		final AutoCompleteTextView input = new AutoCompleteTextView(
+				getActivity());
 
 		ArrayList<String> tags = new ArrayList<String>();
-		
-		new AlertDialog.Builder(getActivity())
-	    .setTitle("Create Tag")
-	    .setMessage(message)
-	    .setView(input)
-	    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-	        public void onClick(DialogInterface dialog, int whichButton) {       		
-	        	value = input.getText();
-		    	if ( input.getText() != null ){
-		    		Tag newTag = new Tag(input.getText().toString());
-		    		getTagMap(getActivity()).addTag(getActivity(), newTag);
-		    		//listOfTags.add(newTag);
 
-		    	}
-		    	
-		    	value = null;
-	        }
-	    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-	        public void onClick(DialogInterface dialog, int whichButton) {
-	            // Do nothing.
-	        }
-	    }).show();
-		
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		if (edit)
+			builder.setTitle("Edit Tag");
+		else
+			builder.setTitle("Create Tag");
+		builder.setMessage(message);
+		builder.setView(input);
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				value = input.getText();
+				if (input.getText() != null) {
+					if (!edit) {
+						Tag newTag = new Tag(input.getText().toString());
+						getTagMap(getActivity()).addTag(getActivity(), newTag);
+						// listOfTags.add(newTag);
+					} else
+						t.rename(getActivity(), input.getText().toString());
+
+				}
+
+				value = null;
+			}
+		}).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				// Do nothing.
+			}
+		});
+		AlertDialog build = builder.create();
+		build.show();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see group5.trackerexpress.TView#update(group5.trackerexpress.TModel)
 	 */
 	@Override
 	public void update(TModel model) {
-		ArrayList<Tag> listOfTags = getTagMap(getActivity()).getTags();	
-		if ( listOfTags == null ){
+		ArrayList<Tag> listOfTags = getTagMap(getActivity()).getTags();
+		if (listOfTags == null) {
 			listOfTags = new ArrayList<Tag>();
 		}
-		MainTagListAdapter a = new MainTagListAdapter( getActivity(), listOfTags );
+		MainTagListAdapter a = new MainTagListAdapter(getActivity(), listOfTags);
 		lv_tag_list.setAdapter(a);
 	}
 }
